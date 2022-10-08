d3d3de41b28b42e4ac9a0128f47aacb56dc4131b
Sigmoidle <71047292+Sigmoidle@users.noreply.github.com>
2022-10-08 18:19:04 +0100

    Initial commit
    This initial commit contains an f# lexical analyser

---
+++ interpreter/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+
+let isBlank c = Char.IsWhiteSpace c
+let isDigit c = Char.IsDigit c
+    
+type Token = Plus | Multiply | LeftBracket | RightBracket | Number
+
+let rec makeTokens (characters: char list) (tokenList: Token list) =
+    match characters with
+    | '+' :: rest -> makeTokens rest (Token.Plus :: tokenList)
+    | '*' :: rest -> makeTokens rest (Token.Multiply :: tokenList)
+    | '(' :: rest -> makeTokens rest (Token.LeftBracket :: tokenList)
+    | ')' :: rest -> makeTokens rest (Token.RightBracket :: tokenList)
+    | whitespace :: rest when isBlank whitespace -> makeTokens rest tokenList
+    | [] -> List.rev(tokenList)
+    | err -> failwith "tokenization error"
+
+let tokenize (inputString: string) =
+    let characters = List.ofArray(inputString.ToCharArray())
+    let tokens = makeTokens characters []
+    tokens
+  
+[<EntryPoint>]  
+let main args=
+    Console.ReadLine() |> tokenize |> printfn "%A"
+    0
+    
+
+
