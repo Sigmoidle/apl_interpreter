4ea786c4bc177955f81f155ae2757bd5719314ce
Sigmoidle <71047292+Sigmoidle@users.noreply.github.com>
2022-10-24 11:52:39 +0100

    Syntax improvement to Lexer

--- apl_compiler/Parser.fs
+++ apl_compiler/Parser.fs
@@ -1,2 +1,18 @@
 ﻿module apl_compiler.Parser
 
+open Lexer
+
+// Backus Nuar From for APL
+
+// <PROGRAM> := <STATEMENT> * <EOF> 
+// <STATEMENT> := ( <ARRAY> * <Dyadic F> | <Monadic F> ) * <ARRAY>
+// <ARRAY> := ( "(" <STATEMENT> ")" | <FLOAT> ) +
+// <ASSIGN> := <IDENTIFIER> "←" <ARRAY> | <STATEMENT>
+// <IDENTIFIER> := "a".."z" | "A".."Z"
+// <Dyadic F> := "Plus(+)"
+// <Monadic F> := "Conjugate(+)"
+
+[<EntryPoint>]
+let main _ =
+    testLexer
+    0
