7bf186164c5c0449c8a0fdb40e19705682c71c23
Sigmoidle <71047292+Sigmoidle@users.noreply.github.com>
2022-10-24 21:45:52 +0100

    BNF implemented in algebraic type system. This will be our syntax tree data structure.

--- apl_compiler/Parser.fs
+++ apl_compiler/Parser.fs
@@ -1,6 +1,5 @@
 ﻿module apl_compiler.Parser
-
-open Lexer
+open apl_compiler.Lexer
 
 // Backus Nuar Form for APL
 (*
@@ -11,7 +10,6 @@ open Lexer
                 | <Expression> "NewLine" <ExpressionList>          
 <Expression> ::=  
                 | <ArrayExpression>
-                | "(" <ExpressionList> ")"
 <ArrayExpression> ::=
                 | <ArrayAtom>
                 | <Function> <ArrayExpression>
@@ -19,7 +17,6 @@ open Lexer
 <ArrayAtom> ::=
                 | "Number"
                 | Strand
-                | "(" <ArrayExpression ")"
 <ArrayNaming> ::= "Identifier" "←" <ArrayExpression>
 <Strand> ::= <ArrayAtom> <Strand>
 <Function> ::= <FunctionAtom>
@@ -27,6 +24,75 @@ open Lexer
 <F> ::= "+"
 *)
 
+type ExpressionList =
+    | NewLine
+    | Expression of Expression
+    | ExpressionNewLine of Expression
+    | ExpressionNewLineExpressionList of Expression * ExpressionList
+    
+and Expression =
+    | ArrayExpression of ArrayExpression
+
+and ArrayExpression =
+    | ArrayAtom of ArrayAtom
+    | FunctionArrayExpression of Function * ArrayExpression
+    | ArrayNaming of ArrayNaming
+    
+and ArrayAtom =
+    | Number of float
+    | Strand of Strand
+    
+and ArrayNaming = Naming of string * ArrayExpression
+
+and Strand = Strand of ArrayAtom * Strand
+
+and Function = Function of FunctionAtom
+
+and FunctionAtom = FunctionAtom of F
+
+and F = Token of PrimitiveFunction
+
+and PrimitiveFunction =
+    | Plus // +
+    // | Hyphen // -
+    // | Multiplication // ×
+    // | Division // ÷
+    // | Asterisk // *
+    // | CircleStar // ⍟
+    // | LeftCeiling // ⌈
+    // | LeftFloor // ⌊
+    // | VerticalBar // |
+    // | ExclamationMark // !
+    // | WhiteCircle // ○
+    // | LessThan // <
+    // | NotGreaterThan // ≤
+    // | Equals // =
+    // | NotLessThan // ≥
+    // | GreaterThan // >
+    // | NotEqual // ≠
+    // | LogicalAnd // ∧
+    // | LogicalOr // ∨
+    // | UpCaretTilde // ⍲
+    // | DownCaretTilde // ⍱
+    // | Tilde // ~
+    // | QuestionMark // ?
+    // | Rho // ⍴
+    // | Iota // ⍳
+    // | SmallElementOf // ∊
+    // | UpwardPointingArrow // ↑
+    // | DownwardPointingArrow // ↓
+    // | DeltaStile // ⍋
+    // | DelStile // ⍒
+    // | CircleStile // ⌽
+    // | CircleBackslash // ⍉
+    // | CircledMinus // ⊖
+    // | Slash
+
+// and Slash =
+//     | Slash // /
+//     | SlashBar // ⌿
+//     | Backslash // \
+//     | BackslashBar // ⍀
 
 
 [<EntryPoint>]
