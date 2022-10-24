05dbe96c05c9a3cf5cfd781a3ba3a3fc93c2185d
Sigmoidle <71047292+Sigmoidle@users.noreply.github.com>
2022-10-24 13:24:09 +0100

    wip bnf

--- apl_compiler/Parser.fs
+++ apl_compiler/Parser.fs
@@ -2,15 +2,24 @@
 
 open Lexer
 
-// Backus Nuar From for APL
-
-// <PROGRAM> := <STATEMENT> * <EOF> 
-// <STATEMENT> := ( <ARRAY> * <Dyadic F> | <Monadic F> ) * <ARRAY>
-// <ARRAY> := ( "(" <STATEMENT> ")" | <FLOAT> ) +
-// <ASSIGN> := <IDENTIFIER> "←" <ARRAY> | <STATEMENT>
-// <IDENTIFIER> := "a".."z" | "A".."Z"
-// <Dyadic F> := "Plus(+)"
-// <Monadic F> := "Conjugate(+)"
+// Backus Nuar Form for APL
+(*
+<PROGRAM> ::= 
+                | (<STATEMENT> | <EXPRESSION>) "End of Tokens" 
+<EXPRESSION> ::= 
+                | (( <ARRAY> <Dyadic F> | <Monadic F> ) <ARRAY>) <EXPRESSION>
+                | (( <ARRAY> <Dyadic F> | <Monadic F> ) <ARRAY>) 
+                | <ARRAY>
+<STATEMENT> ::= 
+                | <ASSIGN>
+<ARRAY> ::= 
+                | "Number" <ARRAY>
+                | "Number"
+<ASSIGN> ::= 
+                | "IDENTIFIER" "←" <EXPRESSION>
+<Dyadic F> ::= "Plus(+)"
+<Monadic F> ::= "Conjugate(+)"
+*)
 
 [<EntryPoint>]
 let main _ =
