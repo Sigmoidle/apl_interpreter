333514c4792306f1bdc02540ea2f622dec10c5e5
Sleeping-Donut <21991990+Sleeping-Donut@users.noreply.github.com>
2022-11-23 02:49:58 +0000

    added parser to evaluate addition expressions

--- apl_compiler/main.fs
+++ apl_compiler/main.fs
@@ -2,8 +2,18 @@
 open apl_compiler.Parser
 open apl_compiler.Lexer
 
+let getInputString() : string = 
+    System.Console.Write("Enter an expression: ")
+    System.Console.ReadLine()
 
 [<EntryPoint>]
 let main _ =
-    testLexer
+    System.Console.WriteLine("Simple Interpreter for APL")
+    //let input = "1 + 1"
+    let input = getInputString()
+    let tokList = lex input
+    let huhList = printTlist tokList;
+    System.Console.WriteLine("-----")
+    let out = parseAndEval tokList
+    System.Console.WriteLine("Result = {0}", snd out)
     0
