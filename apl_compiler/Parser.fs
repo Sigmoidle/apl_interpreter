527a3b9b9a8f7df3d9dccfc96f699f9b7d4a1478
Sigmoidle <71047292+Sigmoidle@users.noreply.github.com>
2022-11-29 21:11:26 +0000

    Slight Clean up of F# code. No logical change.

--- apl_compiler/Parser.fs
+++ apl_compiler/Parser.fs
@@ -1,4 +1,5 @@
 ﻿module apl_compiler.Parser
+
 open Lexer
 
 (*
@@ -11,17 +12,15 @@ open Lexer
 <FUNCTION> ::= "+"
 
  *)
- 
+
 type Program =
     | EndOfFile
     | Statement of Statement
- 
-and Statement =
-    Function of float * Function * float
 
-and Function =
-    Add
-     
+and Statement = Function of float * Function * float
+
+and Function = Add
+
 let private parseError = System.Exception("parse error")
 
 // TODO: make copy of this fn that adds the parts to a parse tree as it
@@ -29,17 +28,21 @@ let private parseError = System.Exception("parse error")
 let parseAndEval tokens =
     let rec Program tokens =
         match tokens with
-        | Token.EndOfFile :: tail -> (tail, 0.0)// Probably shouldn't return 0 here but not sure what val to return
+        | Token.EndOfFile :: tail -> (tail, 0.0) // Probably shouldn't return 0 here but not sure what val to return
         | _ -> Statement tokens
+
     and Statement tokens = (Number >> Function) tokens
+
     and Function (tokens, value) =
         match tokens with
-        | Token.Plus :: tail -> let (tokens, tokenVal) = Number tail
-                                Function (tokens, value + tokenVal)
+        | Token.Plus :: tail ->
+            let tokens, tokenVal = Number tail
+            Function(tokens, value + tokenVal)
         | _ -> (tokens, value)
+
     and Number tokens =
         match tokens with
         | Token.Number value :: tail -> (tail, value)
         | _ -> raise parseError
-    Program tokens
 
+    Program tokens
--- apl_compiler/main.fs
+++ apl_compiler/main.fs
@@ -1,18 +1,17 @@
 ﻿module apl_compiler.main
+
 open apl_compiler.Parser
 open apl_compiler.Lexer
 
-let getInputString() : string = 
+let getInputString () : string =
     System.Console.Write("Enter an expression: ")
     System.Console.ReadLine()
 
 [<EntryPoint>]
 let main _ =
     System.Console.WriteLine("Simple Interpreter for APL")
-    //let input = "1 + 1"
-    let input = getInputString()
-    let tokList = lex input
     System.Console.WriteLine("-----")
-    let out = parseAndEval tokList
+    let out = getInputString() |> lex |> parseAndEval
+    System.Console.WriteLine("-----")
     System.Console.WriteLine("Result = {0}", snd out)
     0
