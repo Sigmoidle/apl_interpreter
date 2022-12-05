09373b972135b71a8c8a4e3366ff542f6a50c704
Sleeping-Donut <21991990+Sleeping-Donut@users.noreply.github.com>
2022-12-05 15:18:49 +0000

    change main to output full array in string form no trim

--- apl_compiler/main.fs
+++ apl_compiler/main.fs
@@ -7,11 +7,14 @@ let getInputString () : string =
     System.Console.Write("Enter an expression: ")
     System.Console.ReadLine()
 
+let fullListToString (values) : string =
+    values |> Seq.map(fun n -> n.ToString()) |> String.concat ", " |> sprintf "[ %s ]"
+
 [<EntryPoint>]
 let main _ =
     System.Console.WriteLine("Simple Interpreter for APL")
     System.Console.WriteLine("-----")
     let out = getInputString() |> lex |> parseAndEval
     System.Console.WriteLine("-----")
-    System.Console.WriteLine("Result = {0}", snd out)
+    System.Console.WriteLine("Result = {0}", snd out |> fullListToString)
     0
