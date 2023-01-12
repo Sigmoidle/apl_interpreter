60acbd8095aa541a2ad0fa7449cba501715b418b
Sigmoidle <71047292+Sigmoidle@users.noreply.github.com>
2023-01-12 15:27:54 +0000

    slight formatting changes

--- apl_compiler/Runtime.fs
+++ apl_compiler/Runtime.fs
@@ -1,7 +1,7 @@
 ﻿module apl_compiler.Runtime
 
 open Parser
-    
+
 let private runtimeError error = System.Exception(error)
 
 let private _Add (list1: float list, list2: float list) =
@@ -21,7 +21,7 @@ let private _Add (list1: float list, list2: float list) =
 
     AddList(list1, list2)
 
-let rec private _Not numList=
+let rec private _Not numList =
     let ContainsOnlyBinaryValue (numList: float list) = numList |> Seq.forall (fun n -> n = 1.0 || n = 0.0)
 
     if not (ContainsOnlyBinaryValue numList) then
@@ -33,31 +33,31 @@ let rec private _Not numList=
         numList |> Seq.map (fun n -> if n = 1.0 then 0.0 else 1.0) |> Seq.toList
 
 
-let runtime program = 
+let runtime program =
     let rec _Program (program, out) =
         match program with
         | Program.EndOfFile -> (program, out)
-        | Program.NewLine(program) -> _Program (program, out)
-        | Program.Statement(statement, program) ->
+        | Program.NewLine program -> _Program (program, out)
+        | Program.Statement (statement, program) ->
             let newOut = _Statement (statement, out)
             _Program (program, newOut)
-    
+
     and _Statement (statement, out) =
         match statement with
-        | MonadicFn(func) -> _MonadicFn (func, out)
-        | DyadicFn(func) -> _DyadicFn (func, out)
-    
+        | MonadicFn func -> _MonadicFn (func, out)
+        | DyadicFn func -> _DyadicFn (func, out)
+
     and _MonadicFn (monadicFn, out) =
         match monadicFn with
-        | Not(maybeChain) -> _MaybeChain(maybeChain, out) |> _Not 
-            
+        | Not maybeChain -> _MaybeChain (maybeChain, out) |> _Not
+
     and _DyadicFn (dyadicFn, out) =
         match dyadicFn with
-        | Add(nList, maybeChain) -> (nList, _MaybeChain(maybeChain, out)) |> _Add 
-        
+        | Add (nList, maybeChain) -> (nList, _MaybeChain (maybeChain, out)) |> _Add
+
     and _MaybeChain (maybeChain, out) =
         match maybeChain with
-        | NoChain(numList) -> numList
-        | Chain(statement) -> _Statement(statement, out)
-        
-    _Program (program, [0])
+        | NoChain numList -> numList
+        | Chain statement -> _Statement (statement, out)
+
+    _Program (program, [ 0 ])
