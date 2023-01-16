35606831359d53cd07b671ba5a0700f844a4c07c
Sleeping-Donut <21991990+Sleeping-Donut@users.noreply.github.com>
2023-01-16 04:30:29 +0000

    change error message for minimum fn

--- apl_interpreter/Runtime.fs
+++ apl_interpreter/Runtime.fs
@@ -187,8 +187,8 @@ let private _Ceiling (list: float list) = List.map (fun (x: float) -> ceil x) li
 
 let private _Maximum (list1: float list, list2: float list) =
     match list1.Length, list2.Length with
-    | 0, _ -> raise <| runtimeError $"The array %A{list1} is empty, the power operation requires numbers"
-    | _, 0 -> raise <| runtimeError $"The array %A{list2} is empty, the power operation requires numbers"
+    | 0, _ -> raise <| runtimeError $"The array %A{list1} is empty, the maximum operation requires numbers"
+    | _, 0 -> raise <| runtimeError $"The array %A{list2} is empty, the maximum operation requires numbers"
     | _, _ when list1.Length <> list2.Length && list1.Length <> 1 && list2.Length <> 1 ->
         raise <| runtimeError $"The array %A{list1} and %A{list2} must be of equal length unless either has only one value"
     | _, _ when list1.Length = 1 || list2.Length = 1 ->
